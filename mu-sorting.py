2b85054545bf697f7762b2ecd0d4c0d35a572db4
Aeron Sor <aeronsor@gmail.com>
2022-10-06 19:06:39 -0300

    dependencies and finished up GUI

--- mu-sorting.py
+++ mu-sorting.py
@@ -1,10 +1,16 @@
+"""
+My First script of this kind, as you can see its messy as hell, but it works :)
+"""
+
 # Innit bruv
 import os
 import glob
 import re
 import shutil
 import tkinter as tk
+from tkinter import messagebox
 import customtkinter 
+
 
 if __name__ == "__main__":
 
@@ -15,74 +21,101 @@ if __name__ == "__main__":
             super().__init__()
 
             # Window properties
-            self.geometry("400x240")
+            self.geometry("450x320")
             self.title("MU Organizer")
             # Elements get buggy if set to dark mode for some reason ????
             self.set_appearance_mode("light")
 
+            # Check variable if program gets closed or not
+            self.killed = False
+
+            self.protocol("WM_DELETE_WINDOW", self.on_close)
+
             self.create_widgets()
 
         def create_widgets(self):
-            # Label
-            label = customtkinter.CTkLabel(master=self, text="Enter your player name:")
-            label.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER)
+            # Path Label 
+            p_label = customtkinter.CTkLabel(master=self, text="Enter Replay folder Path:")
+            p_label.place(relx=0.5, rely=0.2, anchor=customtkinter.CENTER)
+            
+            # Path entry
+            self.path_var = customtkinter.StringVar()
+            p_name_entry = customtkinter.CTkEntry(master=self, textvariable =self.path_var)
+            p_name_entry.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER)
 
-            # Entry
+            # User Label
+            u_label = customtkinter.CTkLabel(master=self, text="Enter your player name:")
+            u_label.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+            # User Entry
             self.name_var = customtkinter.StringVar()
-            name_entry = customtkinter.CTkEntry(master=self, textvariable =self.name_var)
-            name_entry.place(relx =0.5, rely =0.4, anchor=customtkinter.CENTER)
+            u_name_entry = customtkinter.CTkEntry(master=self, textvariable =self.name_var)
+            u_name_entry.place(relx =0.5, rely =0.6, anchor=customtkinter.CENTER)
 
             # Button
             submit_button = customtkinter.CTkButton(master=self, text='Submit', command= self.submit)
-            submit_button.place(relx=0.5, rely=0.6, anchor=customtkinter.CENTER)
+            submit_button.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
 
                
         def submit(self):
             self.name_var = self.name_var.get()
+            self.path_var = self.path_var.get()
             self.destroy()
+
+
+        def on_close(self):
+            self.killed = True
+            self.destroy()
+
 
     app = App()
     app.mainloop()
 
+    killed = app.killed
+    
+    if killed == False:
 
-    # BACKEND #
-    source = './'
+        # BACKEND #
 
-    # Creates list with all the containing MU names
-    matchups = ('ZA', 'FA', 'CH', 'MI', 'VE', 'KL', 'DI', 'JA', 'JO', 'OS', 'AX', 'BA',
-                'RO', 'SL', 'ED', 'AN', 'IN', 'BR', 'TE', 'KY', 'PO', 'MA', 'SO', 'AB', 'JU')
+        # Creates list with all the containing MU names
+        matchups = ('ZA', 'FA', 'CH', 'MI', 'VE', 'KL', 'DI', 'JA', 'JO', 'OS', 'AX', 'BA',
+                    'RO', 'SL', 'ED', 'AN', 'IN', 'BR', 'TE', 'KY', 'PO', 'MA', 'SO', 'AB', 'JU')
 
 
-    # Get name
-    user_name = app.name_var
+        # Get name
+        user_name = str(app.name_var)
 
-    # Create all directories if they do not exist
-    for name in matchups:
-        if not os.path.exists(name):
-            os.mkdir(name)
+        # Get path
+        source = str(app.path_var)
 
-        if not os.path.exists("$Spectate"):
-            os.mkdir("$Spectate")
 
-    # Get Files
-    for file in glob.glob(source + '/*ggr'):
+        # Create all directories if they do not exist
+        for name in matchups:
+            if not os.path.exists(source+name):
+                os.mkdir(source+name)
 
-        # Send Spectate replays to /$Spectate folders
-        if re.search('^((?!Aeron Sor).)*$', file):
-            shutil.copy(file, './$Spectate')
-            print(f"Moved {file} to ./$Spectate")
-        
-        else:
+            if not os.path.exists(source+"$Spectate"):
+                os.mkdir(source+"$Spectate")
 
-            # Send the rest to its appropriate folder
-            for name in matchups:
+        # Get Files
+        for file in glob.glob( source + '/*ggr'):
 
-                if  re.search(f"(?<!{user_name})\({name}\)", file):
-                    shutil.copy(file, source+name)
-                    print(f"Moved {file} to /{name}")
+            # Send Spectate replays to /$Spectate folders
+            if re.search(f'^((?!{user_name}).)*$', file):
+                shutil.copy(file, source+'$Spectate')
+                print(f"Moved {file} to ./$Spectate")
+            
+            else:
 
-        # Cleans the leftover files
-        os.remove(source+file)
+                # Send the rest to its appropriate folder
+                for name in matchups:
+
+                    if  re.search(f"(?<!{user_name})\({name}\)", file):
+                        shutil.copy(file, source+name)
+                        print(f"Moved {file} to /{name}")
+
+            # Cleans the leftover files
+            os.remove(file)
 
 else:
     pass
